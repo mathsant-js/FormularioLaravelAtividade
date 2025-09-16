bba6a32324c3eabb8ecbe3e604581bfc7542b749
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2025-09-16 18:16:55 -0300

    feat: routes has been configured

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View as ContractView;
+use Illuminate\Contracts\View\Factory as FactoryView;
 
 class PrincipalController extends Controller
 {
-    //
+    public function index(): FactoryView|ContractView
+    {
+        return view('principal');
+    }
 }
