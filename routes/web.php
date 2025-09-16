13b69620853ed7b070f3aa9ba519f2057bc5f7a8
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2025-09-16 18:27:00 -0300

    feat: fallback has been added

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+
 Route::get('/', function () {
     return view('contato');
 });
 
+Route::get('/principal', [PrincipalController::class, 'index'])->name('principal');
 Route::get('/contato', [ContatoController::class, 'index'])->name('contato');
+
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="'.route('principal').'">clique aqui</a> para ir para página inicial';
+});
